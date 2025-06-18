c3a640614f4ad1b40271a683cef26de8a846a5b7
natborks <nathanael@moonsquare.co>
2025-06-18 14:38:16 -0230

    add vote endpont and fix terraform deploy

--- terraform/app-module/network.tf
+++ terraform/app-module/network.tf
@@ -10,6 +10,7 @@ resource "aws_lb_target_group" "hiker-votes-tg" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
   target_type = "ip"
+  deregistration_delay = 10
 
   health_check {
     path                = "/"
